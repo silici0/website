493fd5046e1812569ed54caf3d79050078f67da4
Rafael Cunha <silici0@gmail.com>
2021-06-21 21:24:08 -0300

    circles running

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,20 +66,66 @@
             .spin {
                 display: block;
                 animation: spin 10s linear infinite;
+                width: 25em;
+                height: 25em;
+                position: absolute;
+                top: calc(50% - 6em);
+                left: calc(50% - 10em);
             }
             @keyframes spin {
-              0% {
-                transform: rotateZ(0);
-              }
-              100% {
-                transform: rotateZ(360deg);
-              }
+                0% {
+                    transform: rotateZ(0);
+                }
+                100% {
+                    transform: rotateZ(360deg);
+                }
             }
             .circle {
                 border-radius: 50%;
-                left: calc(50% - 6.25em);
-                top: calc(50% - 12.5em);
-                transform-origin: 50% 12.5em;
+                left: calc(50% - 8em);
+                top: calc(50% - 16.7em);
+                transform-origin: 50% 16.5em;
+            }
+            .large.circle {
+                box-shadow: 0 1em 2em rgba(0,0,0,0.5);
+            }
+.one {
+background: rgba(16, 63, 236, 0.75);
+transform: rotateZ(225deg);
+}
+.two {
+background: rgba(37, 172, 162, 0.75);
+transform: rotateZ(180deg);
+}
+.three {
+background: rgba(233, 124, 32, 0.75);
+transform: rotateZ(135deg);
+}
+.four {
+background: rgba(235, 67, 35, 0.75);
+transform: rotateZ(90deg);
+}
+.five {
+background: rgba(190, 28, 65, 0.75);
+transform: rotateZ(45deg);
+}
+.six {
+background: rgba(208, 57, 159, 0.75);
+transform: rotateZ(0);
+}
+.seven {
+background: rgba(150, 32, 198, 0.75);
+transform: rotateZ(-45deg);
+}
+.eight {
+background: rgba(95, 33, 203, 0.75);
+transform: rotateZ(-90deg);
+}
+            .circle, .squircle {
+                width: 15.5em;
+                height: 15.5em;
+                position: absolute;
+                border-top: 0.1em solid rgba(255,255,255,0.4);
             }
         </style>
     </body>
