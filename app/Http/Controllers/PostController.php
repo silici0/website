e5b3c90bb7b51508718881210e12d5b1b34ac310
Rafael Cunha <silici0@gmail.com>
2021-07-06 19:11:12 -0300

    Ajustes em CSS, novas areas... links de redes sociais, envio do form

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,22 +10,10 @@ use Log;
 class PostController extends Controller
 {
     public function enviaContato(Request $request) {
-        Log::info("Ciclo de request sem Queues inicio");
+        // Log::info("Ciclo de request sem Queues inicio");
         $lead = $request->all();
-        var_dump($lead);
         Mail::to('silici0@gmail.com')->send(new SendEmail($lead));
-        if( count(Mail::failures()) > 0 ) {
-
-   echo "There was one or more failures. They were: <br />";
-
-   foreach(Mail::failures() as $email_address) {
-       echo " - $email_address <br />";
-    }
-
-} else {
-    echo "No errors, all sent successfully!";
-}
-        Log::info("Ciclo de request sem Queues fim");
+        // Log::info("Ciclo de request sem Queues fim");
     }
 
 }
